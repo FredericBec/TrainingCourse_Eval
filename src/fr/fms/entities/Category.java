d6a7dc7a6c38107e73b5a1cf217fc20b6f7b38fe
Frederic Bec <bec.frederic90@outlook.fr>
2024-02-06 13:04:38 +0100

    Add component TrainingCourse and Category

--- src/fr/fms/entities/Category.java
+++ src/fr/fms/entities/Category.java
@@ -2,4 +2,43 @@ package fr.fms.entities;
 
 public class Category {
 
+	private int IdCategory;
+	private String CatName;
+	private String Description;
+	
+	public Category(int idCategory, String catName, String description) {
+		super();
+		IdCategory = idCategory;
+		CatName = catName;
+		Description = description;
+	}
+
+	public int getIdCategory() {
+		return IdCategory;
+	}
+
+	public void setIdCategory(int idCategory) {
+		IdCategory = idCategory;
+	}
+
+	public String getCatName() {
+		return CatName;
+	}
+
+	public void setCatName(String catName) {
+		CatName = catName;
+	}
+
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String description) {
+		Description = description;
+	}
+
+	@Override
+	public String toString() {
+		return IdCategory + " - " + CatName + " - " + Description;
+	}
 }
--- src/fr/fms/entities/TrainingCourse.java
+++ src/fr/fms/entities/TrainingCourse.java
@@ -2,4 +2,81 @@ package fr.fms.entities;
 
 public class TrainingCourse {
 
+	private int IdTrainingCourse;
+	private String name;
+	private String description;
+	private int duration;
+	private double price;
+	private int IdCategory;
+	
+	public TrainingCourse(int IdTrainingCourse, String name, String description, int duration, double price) {
+		this.IdTrainingCourse = IdTrainingCourse;
+		this.name = name;
+		this.description = description;
+		this.duration = duration;
+		this.price = price;
+	}
+	
+	public TrainingCourse(int IdTrainingCourse, String name, String description, int duration, double price, int IdCategory) {
+		this.IdTrainingCourse = IdTrainingCourse;
+		this.name = name;
+		this.description = description;
+		this.duration = duration;
+		this.price = price;
+		this.IdCategory = IdCategory;
+	}
+
+	public int getIdTrainingCourse() {
+		return IdTrainingCourse;
+	}
+
+	public void setIdTrainingCourse(int idTrainingCourse) {
+		IdTrainingCourse = idTrainingCourse;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getIdCategory() {
+		return IdCategory;
+	}
+
+	public void setIdCategory(int idCategory) {
+		IdCategory = idCategory;
+	}
+
+	@Override
+	public String toString() {
+		return IdTrainingCourse + " - " + name + " - "
+				+ description + " - " + duration + " - " + price;
+	}
 }
