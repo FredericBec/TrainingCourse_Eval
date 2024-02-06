1d00dedce76ea4925785726fbd8abbc2e4c2fb51
Frederic Bec <bec.frederic90@outlook.fr>
2024-02-06 15:31:35 +0100

    Finalize component Dao and TrainingCourseDao

--- src/fr/fms/dao/Dao.java
+++ src/fr/fms/dao/Dao.java
@@ -1,9 +1,13 @@
 package fr.fms.dao;
 
+import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.logging.Logger;
 
 public interface Dao<T> {
 
+	public static Connection connection = BddConnection.getConnection();
+	public static final Logger logger = Logger.getLogger(Dao.class.getName());
 	public void create(T obj);
 	public T read(int id);
 	public boolean update(T obj);
--- src/fr/fms/dao/TrainingCourseDao.java
+++ src/fr/fms/dao/TrainingCourseDao.java
@@ -1,5 +1,7 @@
 package fr.fms.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -7,27 +9,87 @@ import fr.fms.entities.TrainingCourse;
 
 public class TrainingCourseDao implements Dao<TrainingCourse> {
 
+	private static ArrayList<TrainingCourse> trainingCourses = new ArrayList<TrainingCourse>();
+	
 	@Override
 	public void create(TrainingCourse obj) {
-		
+		String create = "INSERT INTO T_TrainingCourses (Name, Description, Duration, Type, Price) VALUES (?,?,?,?,?);";
+		try(PreparedStatement ps = connection.prepareStatement(create)) {
+			ps.setString(1, obj.getName());
+			ps.setString(2, obj.getDescription());
+			ps.setInt(3, obj.getDuration());
+			ps.setString(4, obj.getType());
+			ps.setDouble(5, obj.getPrice());
+			if(ps.executeUpdate() == 1)
+				System.out.println("La formation a bien été créée");
+		} catch (SQLException e) {
+			logger.severe("Problème de création de la formation : " + e.getMessage());
+		}
 		
 	}
 
 	@Override
 	public TrainingCourse read(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		TrainingCourse trainingCourse = null;
+		String readTc = "SELECT IdTrainingCourse, Name, Description, Duration, Type, Price FROM T_TrainingCourses WHERE IdTrainingCourse = ?;";
+		try(PreparedStatement ps = connection.prepareStatement(readTc)) {
+			ps.setInt(1, id);
+			try(ResultSet resultSet = ps.executeQuery()){
+				if(resultSet.next()) {
+					int rsIdTrainingCourse = resultSet.getInt("IdTrainingCourse");
+					String rsName = resultSet.getString("Name");
+					String rsDescription = resultSet.getString("Description");
+					int rsDuration = resultSet.getInt("Duration");
+					String rsType = resultSet.getString("Type");
+					double rsPrice = resultSet.getDouble("Price");
+					
+					System.out.println("Article trouvé");
+					return trainingCourse = new TrainingCourse(rsIdTrainingCourse, rsName, rsDescription, rsDuration, rsType, rsPrice);
+				}else
+					System.out.println("Article non trouvé");
+			}
+			
+		} catch (SQLException e) {
+			logger.severe("La formation n'existe pas " + e.getMessage());
+		}
+		return trainingCourse;
 	}
 
 	@Override
 	public boolean update(TrainingCourse obj) {
-		// TODO Auto-generated method stub
+		String updateTc = "UPDATE T_TrainingCourses SET IdTrainingCourse = ?, Name = ?, Description = ?, "
+							+ "Duration = ?, Type = ?, Price = ?, IdCategory = ? WHERE IdTrainingCourse = ?;";
+		try(PreparedStatement ps = connection.prepareStatement(updateTc)) {
+			ps.setInt(1, obj.getIdTrainingCourse());
+			ps.setString(2, obj.getName());
+			ps.setString(3, obj.getDescription());
+			ps.setInt(4, obj.getDuration());
+			ps.setString(5, obj.getType());
+			ps.setDouble(6, obj.getPrice());
+			ps.setInt(7, obj.getIdCategory());
+			ps.setInt(8, obj.getIdTrainingCourse());
+			if(ps.executeUpdate() == 1) {
+				System.out.println("La formation a bien été mise à jour");
+				return true;
+			}
+		} catch (Exception e) {
+			logger.severe("Problème à la mise à jour : " + e.getMessage());
+		}
 		return false;
 	}
 
 	@Override
 	public boolean delete(TrainingCourse obj) {
-		// TODO Auto-generated method stub
+		String deleteTc = "DELETE FROM T_TrainingCourses WHERE IdTrainingCourse = ?;";
+		try(PreparedStatement ps = connection.prepareStatement(deleteTc)){
+			ps.setInt(1, obj.getIdTrainingCourse());
+			if(ps.executeUpdate() == 1) {
+				System.out.println("Suppréssion de la formation effectuée");
+				return true;
+			}
+		}catch(SQLException e) {
+			logger.severe("Problème de suppression de la formation : " + e.getMessage());
+		}
 		return false;
 	}
 
@@ -35,11 +97,21 @@ public class TrainingCourseDao implements Dao<TrainingCourse> {
 	public ArrayList<TrainingCourse> readAll() {
 		String requestSql = "SELECT * FROM T_TrainingCourses;";
 		try(Statement st = connection.createStatement()) {
-			
+			try(ResultSet resultSet = st.executeQuery(requestSql)){
+				while(resultSet.next()) {
+					int rsIdTrainingCourse = resultSet.getInt("IdTrainingCourse");
+					String rsName = resultSet.getString("Name");
+					String rsDescription = resultSet.getString("Description");
+					int rsDuration = resultSet.getInt("Duration");
+					String rsType = resultSet.getString("Type");
+					double rsPrice = resultSet.getDouble("Price");
+					trainingCourses.add(new TrainingCourse(rsIdTrainingCourse, rsName, rsDescription, rsDuration, rsType, rsPrice));
+				}
+			}
 		} catch (SQLException e) {
-			// TODO: handle exception
+			logger.severe("Problème d'accès aux données : " + e.getMessage());
 		}
-		return null;
+		return trainingCourses;
 	}
 
 	
