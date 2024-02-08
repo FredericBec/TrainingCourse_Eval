f538caf7ee724c6f0c336d250f4497bf9aa23dd9
Frederic Bec <bec.frederic90@outlook.fr>
2024-02-08 12:05:53 +0100

    Add retrieve idOrder to create

--- src/fr/fms/dao/OrderDao.java
+++ src/fr/fms/dao/OrderDao.java
@@ -2,7 +2,9 @@ package fr.fms.dao;
 
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import fr.fms.entities.Order;
@@ -12,13 +14,17 @@ public class OrderDao implements Dao<Order>{
 	@Override
 	public void create(Order obj) {
 		String createOrder = "INSERT INTO T_Orders (Name, TotalAmount, DateOrder, IdCustomer) VALUES (?,?,?,?);";
-		try(PreparedStatement ps = connection.prepareStatement(createOrder)) {
+		try(PreparedStatement ps = connection.prepareStatement(createOrder, Statement.RETURN_GENERATED_KEYS)) {
 			ps.setString(1, obj.getName());
 			ps.setDouble(2, obj.getTotalAmount());
-			ps.setDate(3, (Date) obj.getDate());
+			ps.setDate(3, new Date(obj.getDate().getTime()));
 			ps.setInt(4, obj.getIdCustomer());
 			if(ps.executeUpdate() == 1)
 				System.out.println("La commande a bien été créée");
+			try(ResultSet generetadKeySet = ps.getGeneratedKeys()){
+				if(generetadKeySet.next())
+					obj.setIdOrder(generetadKeySet.getInt(1));
+			}
 		} catch (SQLException e) {
 			logger.severe("Problème de création de la commande : " + e.getMessage());
 		}
